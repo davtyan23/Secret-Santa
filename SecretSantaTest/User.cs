5fa8e25f7aeff492d68f266c0784cbc2fa104532
mariadavtyan <maria.davtyan2006@gmail.com>
2024-09-10 13:38:57 +0400

    User class has been created.

---
+++ SecretSantaTest/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSantaTest
+{
+    public class Users
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string number { get; set; }
+
+    }
+}
+
+
